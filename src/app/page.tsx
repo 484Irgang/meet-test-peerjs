c0688f69aa5567d6a6042464a77c27e801a0014d
Guilherme Irgang <484gui@gmail.com>
2024-10-23 17:30:29 -0300

    --refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="w-full h-full bg-dark-300 flex flex-1 items-center flex-col relative">
-      <div className="mt-[20vh] p-4 rounded-lg bg-dark-200 flex items-center justify-center flex-col gap-y-4 border border-neutral-1000">
+      <div className="mt-[18vh] p-4 rounded-lg bg-dark-200 flex items-center justify-center flex-col gap-y-4 border border-neutral-1000">
+        <h1 className="font-bold text-brand-600">DWV MEET</h1>
         <h1 className="text-[1.75rem] font-bold">Bem vindo</h1>
         <div className="w-full min-w-60 flex flex-col gap-y-2">
           <p className="font-bold text-sm text-neutral-400">
@@ -21,7 +22,7 @@ export default function Home() {
       </div>
       <img
         alt="background"
-        className="absolute bottom-0 w-full max-w-[30rem] object-contain opacity-50"
+        className="absolute bottom-0 w-full max-w-[40rem] object-contain opacity-50"
         src="background_gray.svg"
       />
     </div>
