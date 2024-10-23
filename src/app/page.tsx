49bf2df6bef74b2085b9919746e7edfdd4143140
Guilherme Irgang <484gui@gmail.com>
2024-10-23 17:28:19 -0300

    style: change oage styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full h-full bg-dark-300 flex flex-1 items-center justify-center flex-col">
-      <div className="m-auto p-4 rounded-lg bg-dark-200 flex items-center justify-center flex-col gap-y-4">
+    <div className="w-full h-full bg-dark-300 flex flex-1 items-center flex-col relative">
+      <div className="mt-[20vh] p-4 rounded-lg bg-dark-200 flex items-center justify-center flex-col gap-y-4 border border-neutral-1000">
         <h1 className="text-[1.75rem] font-bold">Bem vindo</h1>
         <div className="w-full min-w-60 flex flex-col gap-y-2">
           <p className="font-bold text-sm text-neutral-400">
@@ -9,7 +9,7 @@ export default function Home() {
           </p>
           <input
             type="text"
-            className="w-full rounded-sm border border-neutral-900 bg-dark-100 h-8"
+            className="w-full rounded-sm border border-neutral-900 bg-dark-100 h-8 outline-none pl-2"
           />
           <button className="w-full h-[2.75rem] rounded-sm bg-brand-600 mt-4">
             Entrar
@@ -19,6 +19,11 @@ export default function Home() {
           </button>
         </div>
       </div>
+      <img
+        alt="background"
+        className="absolute bottom-0 w-full max-w-[30rem] object-contain opacity-50"
+        src="background_gray.svg"
+      />
     </div>
   );
 }
